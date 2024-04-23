19a8daf307fd2fc3c242215adcf762c8340946c7
wildan <21523222@students.uii.ac.id>
2024-04-23 17:48:09 +0700

    Method/Verb and Routing

--- server.js
+++ server.js
@@ -3,12 +3,15 @@
 // 2. Using the module (npm install @hapi/hapi)
 
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
         host: 'localhost',
     });
+
+    server.route(routes);
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
